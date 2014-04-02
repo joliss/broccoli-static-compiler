097e8e68fbb8eb2eb6581e2465577b9be05b2383
Jo Liss <joliss42@gmail.com>
2014-04-02 09:48:09 +0000

    Use new broccoli-writer base class

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 var path = require('path')
 var mkdirp = require('mkdirp')
 var helpers = require('broccoli-kitchen-sink-helpers')
-var Transform = require('broccoli-transform')
+var Writer = require('broccoli-writer')
 
 module.exports = StaticCompiler
-StaticCompiler.prototype = Object.create(Transform.prototype)
+StaticCompiler.prototype = Object.create(Writer.prototype)
 StaticCompiler.prototype.constructor = StaticCompiler
 function StaticCompiler (inputTree, options) {
   if (!(this instanceof StaticCompiler)) return new StaticCompiler(inputTree, options)
   this.inputTree = inputTree
-  this.options = options
+  this.options = options || {}
 }
 
-StaticCompiler.prototype.transform = function (srcDir, destDir) {
-  if (this.options.files == null) {
-    helpers.linkRecursivelySync(
-      path.join(srcDir, this.options.srcDir),
-      path.join(destDir, this.options.destDir))
-  } else {
-    var files = helpers.multiGlob(this.options.files, {
-      cwd: path.join(srcDir, this.options.srcDir)
-    })
-    for (var i = 0; i < files.length; i++) {
-      mkdirp.sync(path.join(destDir, this.options.destDir, path.dirname(files[i])))
-      helpers.linkAndOverwrite(
-        path.join(srcDir, this.options.srcDir, files[i]),
-        path.join(destDir, this.options.destDir, files[i]))
+StaticCompiler.prototype.write = function (readTree, destDir) {
+  var self = this
+
+  return readTree(this.inputTree).then(function (srcDir) {
+    if (self.options.files == null) {
+      helpers.linkRecursivelySync(
+        path.join(srcDir, self.options.srcDir),
+        path.join(destDir, self.options.destDir))
+    } else {
+      var files = helpers.multiGlob(self.options.files, {
+        cwd: path.join(srcDir, self.options.srcDir)
+      })
+      for (var i = 0; i < files.length; i++) {
+        mkdirp.sync(path.join(destDir, self.options.destDir, path.dirname(files[i])))
+        helpers.linkAndOverwrite(
+          path.join(srcDir, self.options.srcDir, files[i]),
+          path.join(destDir, self.options.destDir, files[i]))
+      }
     }
-  }
-  // This method is synchronous, so we don't need to return a promise here
+  })
 }
